5cb018b9e51b84207664f53f8e89a03a04bcaf81
Luiz Lima <umluizlima@gmail.com>
2021-12-12 17:52:57 -0300

    Update texts

--- src/App.js
+++ src/App.js
@@ -166,15 +166,15 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">Meta Creatures</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Each unique. Each beautiful. Mint yours.
           </p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
             <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-              Mint NFT
+              Mint Meta Creature
             </button>
           )}
           <div>
